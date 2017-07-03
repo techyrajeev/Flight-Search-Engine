26441bafff05c5f94d32dfe349f51e5e6c6ed359
rajeev_mishra <rajeev.mishra@wirelesslylinked.com>
2017-07-03 17:05:22 +0530

    Fix public path issue in webpack prod config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path       : path.join(__dirname, 'public'),
     filename   : 'bundle.js',
-    publicPath : '/public/'
+    publicPath : '/'
   },
 
   plugins: [
